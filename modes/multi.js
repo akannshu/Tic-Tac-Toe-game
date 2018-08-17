53e1359b6ffe4a370aa44316bc33adef216a2a66
Akanshu <37223446+akannshu@users.noreply.github.com>
2018-08-17 09:35:05 +0530

    Add files via upload

---
+++ modes/multi.js
@@ -0,0 +1,384 @@
+var sqr1;
+var sqr2;
+var sqr3;
+var sqr4;
+var sqr5;
+var sqr6;
+var sqr7;
+var sqr8;
+var sqr9;
+var sqr1T = 0;
+var sqr2T = 0;
+var sqr3T = 0;
+var sqr4T = 0;
+var sqr5T = 0;
+var sqr6T = 0;
+var sqr7T = 0;
+var sqr8T = 0;
+var sqr9T = 0;
+var moveCount = 0;
+var turn = 1;
+
+function vari()
+{
+sqr1 = document.tic.sqr1.value
+sqr2 = document.tic.sqr2.value
+sqr3 = document.tic.sqr3.value
+sqr4 = document.tic.sqr4.value
+sqr5 = document.tic.sqr5.value
+sqr6 = document.tic.sqr6.value
+sqr7 = document.tic.sqr7.value
+sqr8 = document.tic.sqr8.value
+sqr9 = document.tic.sqr9.value
+}
+
+function player1Check()
+{
+  if(sqr1 == " X " && sqr2 == " X " && sqr3 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr4 == " X " && sqr5 == " X " && sqr6 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr7 == " X " && sqr8 == " X " && sqr9 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr1 == " X " && sqr5 == " X " && sqr9 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr1 == " X " && sqr4 == " X " && sqr7 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr2 == " X " && sqr5 == " X " && sqr8 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr3 == " X " && sqr6 == " X " && sqr9 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr1 == " X " && sqr5 == " X " && sqr9 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else if(sqr3 == " X " && sqr5 == " X " && sqr7 == " X ")
+  {
+    alert("Player 1 wins!")
+    reset()
+  }
+  else
+  {
+    player2Check()
+    drawCheck()
+  }
+}
+
+function player2Check()
+{
+  vari()
+  drawCheck()
+  if(sqr1 == " O " && sqr2 == " O " && sqr3 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr4 == " O " && sqr5 == " O " && sqr6 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr7 == " O " && sqr8 == " O " && sqr9 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr1 == " O " && sqr5 == " O " && sqr9 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr1 == " O " && sqr4 == " O " && sqr7 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr2 == " O " && sqr5 == " O " && sqr8 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr3 == " O " && sqr6 == " O " && sqr9 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr1 == " O " && sqr5 == " O " && sqr9 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+  else if(sqr3 == " O " && sqr5 == " O " && sqr7 == " O ")
+  {
+    alert("Player 2 wins!")
+    reset()
+  }
+}
+
+function drawCheck()
+{
+  vari()
+  moveCount = sqr1T + sqr2T + sqr3T + sqr4T + sqr5T + sqr6T + sqr7T + sqr8T + sqr9T
+  if(moveCount == 9)
+  {
+    reset()
+    alert("Draw")
+  }
+}
+
+function reset()
+{
+  document.tic.sqr1.value = "     "
+  document.tic.sqr2.value = "     "
+  document.tic.sqr3.value = "     "
+  document.tic.sqr4.value = "     "
+  document.tic.sqr5.value = "     "
+  document.tic.sqr6.value = "     "
+  document.tic.sqr7.value = "     "
+  document.tic.sqr8.value = "     "
+  document.tic.sqr9.value = "     "
+  sqr1T = 0;
+  sqr2T = 0;
+  sqr3T = 0;
+  sqr4T = 0;
+  sqr5T = 0;
+  sqr6T = 0;
+  sqr7T = 0;
+  sqr8T = 0;
+  sqr9T = 0;
+  vari()
+  turn = 1;
+  moveCount = 0;
+}
+
+function torn(){
+  if(turn == 1){
+    document.getElementById("tomb").innerHTML = "Player 1 Turn";
+  }
+  else if (turn == 2) {
+    document.getElementById("tomb").innerHTML = "Player 2 Turn";
+  }
+
+}
+
+function begining1(){
+  if(document.tic.sqr1.value == '     ' && turn == 1)
+  {
+    document.tic.sqr1.value = ' X ';
+    sqr1T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr1.value == '     ' && turn == 2) {
+    document.tic.sqr1.value = ' O ';
+    sqr1T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining2(){
+  if(document.tic.sqr2.value == '     ' && turn == 1)
+  {
+    document.tic.sqr2.value = ' X ';
+    sqr2T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr2.value == '     ' && turn == 2) {
+    document.tic.sqr2.value = ' O ';
+    sqr2T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining3(){
+  if(document.tic.sqr3.value == '     ' && turn == 1)
+  {
+    document.tic.sqr3.value = ' X ';
+    sqr3T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr3.value == '     ' && turn == 2) {
+    document.tic.sqr3.value = ' O ';
+    sqr3T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining4(){
+  if(document.tic.sqr4.value == '     ' && turn == 1)
+  {
+    document.tic.sqr4.value = ' X ';
+    sqr4T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr4.value == '     ' && turn == 2) {
+    document.tic.sqr4.value = ' O ';
+    sqr4T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining5(){
+  if(document.tic.sqr5.value == '     ' && turn == 1)
+  {
+    document.tic.sqr5.value = ' X ';
+    sqr5T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr5.value == '     ' && turn == 2) {
+    document.tic.sqr5.value = ' O ';
+    sqr5T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining6(){
+  if(document.tic.sqr6.value == '     ' && turn == 1)
+  {
+    document.tic.sqr6.value = ' X ';
+    sqr6T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr6.value == '     ' && turn == 2) {
+    document.tic.sqr6.value = ' O ';
+    sqr6T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining7(){
+  if(document.tic.sqr7.value == '     ' && turn == 1)
+  {
+    document.tic.sqr7.value = ' X ';
+    sqr7T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr7.value == '     ' && turn == 2) {
+    document.tic.sqr7.value = ' O ';
+    sqr7T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining8(){
+  if(document.tic.sqr8.value == '     ' && turn == 1)
+  {
+    document.tic.sqr8.value = ' X ';
+    sqr8T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr8.value == '     ' && turn == 2) {
+    document.tic.sqr8.value = ' O ';
+    sqr8T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function begining9(){
+  if(document.tic.sqr9.value == '     ' && turn == 1)
+  {
+    document.tic.sqr9.value = ' X ';
+    sqr9T = 1; turn = 2;
+    vari();
+    player1Check();
+    torn();
+  }
+  else if (document.tic.sqr9.value == '     ' && turn == 2) {
+    document.tic.sqr9.value = ' O ';
+    sqr9T = 1; turn = 1;
+    vari();
+    player1Check();
+    torn();
+  }
+   drawCheck();
+}
+
+function reset()
+{
+  document.tic.sqr1.value = "     "
+  document.tic.sqr2.value = "     "
+  document.tic.sqr3.value = "     "
+  document.tic.sqr4.value = "     "
+  document.tic.sqr5.value = "     "
+  document.tic.sqr6.value = "     "
+  document.tic.sqr7.value = "     "
+  document.tic.sqr8.value = "     "
+  document.tic.sqr9.value = "     "
+  sqr1T = 0;
+  sqr2T = 0;
+  sqr3T = 0;
+  sqr4T = 0;
+  sqr5T = 0;
+  sqr6T = 0;
+  sqr7T = 0;
+  sqr8T = 0;
+  sqr9T = 0;
+  vari()
+  turn = 1;
+  moveCount = 0;
+  document.getElementById("tomb").innerHTML="Let's Play Again";
+}
+function description(){
+   document.getElementById("des").innerHTML = " ";
+}
